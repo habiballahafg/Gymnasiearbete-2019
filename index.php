d2de7728b3815829b505cece9d0c91a0e4f0d28c
Hezarehee <Hezarehee@outlook.com>
2019-02-19 12:02:33 +0100

    Backup our index file

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Grand Hotel, Book online your room here!</title>
+    <!-- CSS Assets -->
+    <link rel="stylesheet" href="asset/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="asset/main.css" type="text/css">
+</head>
+<body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+            <a class="navbar-brand order-1" href="#">Grand Hotel</a>
+                <span class="order-2 ">login or signup</span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+    <div class="container">
+        <header>
+            <h1>Gamla Stan (Old town), The capital of wikings</h1>
+            <button><a href="#">Read more ...</a></button>
+        </header>
+        <hr>
+        <section>
+            <div class="booking ">
+                <form action="#" class="form-group">
+                    <label for="room">Room no:</label>
+                    <select class="form-control-sm">
+                        <option value="one">One</option>
+                    </select>
+                </form>
+            </div>
+        </section>
+        <hr>
+        <div class="jumbotron">
+            <hr2>Follow us on Social Media</hr2>
+        </div>
+        <hr>
+        <footer>
+            <p>All right reserved, &copy; Copyright 2019</p>
+        </footer>
+    </div>
+<!-- JavaScript Assets -->
+<script language="JavaScript" src="asset/jquery.js"></script>
+<script languege="JavaScript" src="asset/bootstrap.js"></script>
+<script language="JavaScript" src="asset/mainJS.js"></script>
+<noscript>The JavaScript is not activated, please activate the JavaScript. Thanks!</noscript>
+</body>
+</html>
